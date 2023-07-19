7fa2a60192a7fd65bb6d9ef1f87000ff15c5d6d5
DESKTOP-N74PNCK\yukhg <yukhgcsh@gmail.com>
2023-07-20 01:37:45 +0900

    株式の収支管理用アプリケーションのプロジェクトを作成

---
+++ src/StockManager.Web/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
+using StockManager.Core.Repositories;
+using StockManager.Core.Services;
+using StockManager.Core.Utils;
+using System.Runtime.CompilerServices;
+
+namespace StockManager.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddMudServices();
+            builder.Services.AddSingleton<DashboardService>();
+            builder.Services.AddSingleton<FundsService>();
+            builder.Services.AddSingleton<StockTransactionService>();
+            builder.Services.AddSingleton<DividendService>();
+            builder.Services.AddSingleton<IFundsRepository, DatabaseFundsRepository>();
+            builder.Services.AddSingleton<IStockHistoryRepository, DatabaseStockHistoryRepository>();
+            builder.Services.AddSingleton<IStockRepository, DatabaseStockRepository>();
+            builder.Services.AddAutoMapper(typeof(MapperProfile));
+
+            if (!builder.Environment.IsProduction())
+            {
+                builder.Services.AddSingleton<IFundsRepository, StubFundsRepository>();
+                builder.Services.AddSingleton<IStockHistoryRepository, StubStockHistoryRepository>();
+                builder.Services.AddSingleton<IStockRepository, StubStockRepository>();
+            }
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
