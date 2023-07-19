a156809006bbf15fca557ce605cb3f46dade7921
DESKTOP-N74PNCK\yukhg <yukhgcsh@gmail.com>
2023-07-20 01:44:24 +0900

    AutoMapper用のプロファイルクラスを追加

---
+++ src/StockManager.Core/Utils/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using StockManager.Core.Entities;
+using StockManager.Core.InputModels;
+using StockManager.Core.OutputModels;
+
+namespace StockManager.Core.Utils
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<HoldingStockEntity, HoldingStock>();
+            CreateMap<SoldStockEntity, SoldStock>();
+            CreateMap<StockTransaction, TransactionHistoryEntity>();
+            CreateMap<DividendHistory, DividendEntity>();
+            CreateMap<StockTransaction, HoldingStockEntity>();
+        }
+    }
+}
